441c60ed7769222da9c9ce1789abb5ac80b0eb17
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2017-05-10 23:51:17 +0800

    0.0.4

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-/*istanbul ignore next*/'use strict';
-
-var utils = require('ntils');
+const utils = require('ntils');
 
 /**
  * 定义正则表达式常量
  */
-var PLACE_HOLDER_EXPR = /\{.+?\}/gim;
-var COLLECT_EXPR_STR = '([^\\/]+)';
-var GREEDY_COLLECT_EXPR_STR = '(.+)';
+const PLACE_HOLDER_EXPR = /\{.+?\}/gim;
+const COLLECT_EXPR_STR = '([^\\/]+)';
+const GREEDY_COLLECT_EXPR_STR = '(.+)';
 
 /**
  * 定义路由实例扩展 __proto__
  **/
-var routeInstanceProto = {};
+const routeInstanceProto = {};
 
 /**
  * 生成 action URL
@@ -20,8 +18,8 @@ var routeInstanceProto = {};
  * @return {String} 对应的 path
  **/
 routeInstanceProto.actionUrl = function (action) {
-  var self = this;
-  var actionUrl = self.withoutActionUrl + '/' + action;
+  let self = this;
+  let actionUrl = self.withoutActionUrl + '/' + action;
   actionUrl = actionUrl.replace(/\/\//igm, '/');
   return actionUrl;
 };
@@ -33,7 +31,7 @@ routeInstanceProto.actionUrl = function (action) {
  * @returns {void} 无返回
  */
 function Router(routes, options) {
-  var self = this;
+  let self = this;
   options = options || {};
   self.options = options;
   self.table = [];
@@ -48,9 +46,9 @@ function Router(routes, options) {
  * @returns {Object} 占符符信息对象
  **/
 Router.prototype._parseKeyDef = function (_keyDefStr) {
-  var keyDefStr = _keyDefStr.substring(1, _keyDefStr.length - 1);
-  var keyDefParts = keyDefStr.split(':');
-  var keyDef = {};
+  let keyDefStr = _keyDefStr.substring(1, _keyDefStr.length - 1);
+  let keyDefParts = keyDefStr.split(':');
+  let keyDef = {};
   keyDef.name = keyDefParts[0];
   if (keyDef.name[0] == '*') {
     keyDef.greedy = true;
@@ -68,23 +66,23 @@ Router.prototype._parseKeyDef = function (_keyDefStr) {
  * @returns {void} 无返回
  */
 Router.prototype.addOne = function (route) {
-  var self = this;
+  let self = this;
   if (!route || !route.pattern) return;
   //取到所有路由key
   PLACE_HOLDER_EXPR.lastIndex = 0;
-  var keyDefs = route.pattern.match(PLACE_HOLDER_EXPR) || [];
+  let keyDefs = route.pattern.match(PLACE_HOLDER_EXPR) || [];
   route.keys = {};
   //初始化 url 匹配测试表达式字符串
-  var exprStr = '^' + route.pattern + '$';
+  let exprStr = '^' + route.pattern + '$';
   utils.each(keyDefs, function (i) {
     //处理 key 定义
-    var keyDef = self._parseKeyDef(keyDefs[i]);
+    let keyDef = self._parseKeyDef(keyDefs[i]);
     route.keys[keyDef.name] = {
       index: i,
       expr: keyDef.expr
     };
     //将 'key 占位符' 的表达式，替换为 '提交值的正则表达式'
-    var collectExprStr = keyDef.greedy ? GREEDY_COLLECT_EXPR_STR : COLLECT_EXPR_STR;
+    let collectExprStr = keyDef.greedy ? GREEDY_COLLECT_EXPR_STR : COLLECT_EXPR_STR;
     exprStr = exprStr.replace(keyDefs[i], collectExprStr);
   });
   //生成 url 匹配测试表达式
@@ -107,15 +105,15 @@ Router.prototype.addOne = function (route) {
  * @returns {void} 无返回
  */
 Router.prototype.add = function (routes) {
-  var self = this;
+  let self = this;
   utils.each(routes, function (_name, _route) {
     //判断是字符串还是一个对象，并都将 _route 转为对象
-    var route = utils.isString(_route) ? {
+    let route = utils.isString(_route) ? {
       'target': _route
     } : _route;
     //尝试从名称中解析出 method 和 pattern
-    var name = (_name || '/').toString();
-    var nameParts = name.split(' ');
+    let name = (_name || '/').toString();
+    let nameParts = name.split(' ');
     if (nameParts.length > 1) {
       route.methods = nameParts[0].split(',');
       route.pattern = route.pattern || nameParts[1];
@@ -125,7 +123,7 @@ Router.prototype.add = function (routes) {
     //解析 controller 和 action
     //target 和 controller 不可同时配置，target 可以为 'controller action' 这样的格式
     if (route.target) {
-      var targetParts = route.target.split(' ');
+      let targetParts = route.target.split(' ');
       route.controller = route.controller || targetParts[0];
       route.action = route.action || targetParts[1];
     }
@@ -141,7 +139,9 @@ Router.prototype.add = function (routes) {
  * @returns {Object} 解析后路由项
  **/
 Router.prototype._parseDynamicAction = function (route) {
-  if (route && route.action && route.action.indexOf('{') > -1) {
+  if (route &&
+    route.action &&
+    route.action.indexOf('{') > -1) {
     utils.each(route.params, function (key, val) {
       route.action = utils.replace(route.action, '{' + key + '}', val);
     });
@@ -157,13 +157,13 @@ Router.prototype._parseDynamicAction = function (route) {
  * @returns {Object} 路由实例
  **/
 Router.prototype._createRouteInstance = function (srcRoute, url, params) {
-  var self = this;
-  var routeInstance = {
+  let self = this;
+  let routeInstance = {
     __proto__: srcRoute
   };
   routeInstance.params = params;
   if (routeInstance.action) {
-    var urlParts = url.split('/');
+    let urlParts = url.split('/');
     routeInstance.withoutActionUrl = urlParts.slice(0, urlParts.length - 1);
   } else {
     routeInstance.withoutActionUrl = url;
@@ -179,8 +179,8 @@ Router.prototype._createRouteInstance = function (srcRoute, url, params) {
  * @returns {Route} 路由实体
  */
 Router.prototype.get = function (url, handleActionFromUrl) {
-  var self = this;
-  var routeArray = [];
+  let self = this;
+  let routeArray = [];
   if (utils.isNull(url)) {
     return routeArray;
   }
@@ -190,10 +190,10 @@ Router.prototype.get = function (url, handleActionFromUrl) {
     if (!route.expr.test(url)) return;
     //通过子表达式 '正则的()' 取值
     route.expr.lastIndex = 0;
-    var values = route.expr.exec(url);
+    let values = route.expr.exec(url);
     //生成 params
-    var params = {};
-    var failed = utils.each(route.keys, function (key, keyDef) {
+    let params = {};
+    let failed = utils.each(route.keys, function (key, keyDef) {
       params[key] = values[keyDef.index + 1];
       if (!keyDef.expr) return;
       keyDef.expr.lastIndex = 0;
@@ -205,10 +205,11 @@ Router.prototype.get = function (url, handleActionFromUrl) {
     routeArray.push(self._createRouteInstance(route, url, params));
   });
   //确定 parseActionFromUrl 的值
-  handleActionFromUrl = utils.isNull(handleActionFromUrl) ? self.options.parseActionFromUrl : handleActionFromUrl;
+  handleActionFromUrl = utils.isNull(handleActionFromUrl) ?
+    self.options.parseActionFromUrl : handleActionFromUrl;
   //如果需要 parseActionFromUrl
   if (handleActionFromUrl) {
-    var _routeArray = self._getForActionFromUrl(url);
+    let _routeArray = self._getForActionFromUrl(url);
     routeArray.push.apply(routeArray, _routeArray);
   }
   return routeArray;
@@ -220,21 +221,21 @@ Router.prototype.get = function (url, handleActionFromUrl) {
  * @returns {Object} 路由实例
  **/
 Router.prototype._getForActionFromUrl = function (url) {
-  var self = this;
+  let self = this;
   /*
   一是在如果直接匹配不成功时，才将 “/” 分隔的最后一个 “字串” 当作 action 进行再一次匹配
   */
-  var urlParts = url.split('/');
-  var lastIndex = urlParts.length - 1;
-  var action = urlParts[lastIndex];
+  let urlParts = url.split('/');
+  let lastIndex = urlParts.length - 1;
+  let action = urlParts[lastIndex];
   //检查分解出来的 action 是否合法
   if (action === '' || action.indexOf('.') > -1) {
     return null;
   }
-  var ctrlRouteUrl = urlParts.slice(0, lastIndex).join('/');
+  let ctrlRouteUrl = urlParts.slice(0, lastIndex).join('/');
   if (ctrlRouteUrl === '') ctrlRouteUrl = '/';
-  var ctrlRouteArray = self.get(ctrlRouteUrl, false) || [];
-  var routeArray = ctrlRouteArray.filter(function (route) {
+  let ctrlRouteArray = self.get(ctrlRouteUrl, false) || [];
+  let routeArray = ctrlRouteArray.filter(function (route) {
     /**
      * 从 URL 分解出来的 action 不可能是动态的 action
      * route.action 没有指定时才能作为 parseAction 的合法 route
